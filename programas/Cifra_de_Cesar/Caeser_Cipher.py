dd7ee09fb2650620e6d7f223a6ba983662d9d579
João Guedes <joaopedrochg@gmail.com>
2025-02-18 22:38:56 -0300

    Criando um programa para criptografar uma mensagem utilizando a cifra de César.

---
+++ programas/Cifra_de_Cesar/Caeser_Cipher.py
@@ -0,0 +1,14 @@
+texto = input("Insira uma mensagem: ")
+cipher = ''
+
+for char in texto:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char)+1
+    if code > ord('Z'):
+        code = ord('A')
+    cipher += chr(code)
+
+print(f'Texto criptografado: {cipher}')
+
